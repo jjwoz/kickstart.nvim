f90f02d6a0e66bc35a9193549f8904d9240d309c
jjwoz <jwozniak91@gmail.com>
2024-05-09 06:51:27 -0400

    added better one dark theme. updated harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -11,28 +11,9 @@ return {
       harpoon:list():add()
     end)
     -- basic telescope configuration
-    local conf = require('telescope.config').values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      require('telescope.pickers')
-        .new({}, {
-          prompt_title = 'Harpoon',
-          finder = require('telescope.finders').new_table {
-            results = file_paths,
-          },
-          previewer = conf.file_previewer {},
-          sorter = conf.generic_sorter {},
-        })
-        :find()
-    end
-
     vim.keymap.set('n', '<C-e>', function()
-      toggle_telescope(harpoon:list())
-    end, { desc = 'Open harpoon window' })
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
 
     vim.keymap.set('n', '<C-z>', function()
       harpoon:list():select(1)
